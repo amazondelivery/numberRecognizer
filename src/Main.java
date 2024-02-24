bb2c5b214554031d558a138249eb2cc2a643fe93
Vikram Balaji <mr.vikram.balaji@gmail.com>
2024-02-23 22:45:56 -0600

    first grommit no wallace
    commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/* Sources of ideas behind this implementation:
+        http://neuralnetworksanddeeplearning.com/
+*/
+
+//my initial goal of this project was to initially be a number recognizer, but considering thats.. the exact project
+//the learning material covers, i feel like that would be kind of lame. so the name will stay "numberRecognizer" but
+//ill focus it on some cool numbers because theres lots of cool numbers out there.
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello!");
+    }
+}
